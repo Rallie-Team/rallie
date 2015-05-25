03b27c5481e7cd38d519aa77caa8b55d11307c32
Steven Shyun <stevenshuhyow@yahoo.com>
2015-05-25 11:42:40 -0700

    Merged gulp files

--- Gulpfile.js
+++ Gulpfile.js
@@ -18,8 +18,56 @@ var bundle = function() {
     .pipe(gulp.dest('client/public/js'));
 };
 
+var paths = {
+  images: 'client/public/assets/img/*.*',
+  styles: 'client/public/assets/css/*.css',
+  html: ['client/public/index.html','client/public/app/**/*.html'],
+  server: 'server/**/*.js'
+};
+
+gulp.task('styles', function() {
+  return gulp.src([
+    paths.styles
+  ])
+  .pipe(livereload());
+});
+
+gulp.task('html', function() {
+  return gulp.src(
+    paths.html
+  )
+  .pipe(livereload());
+});
+
+gulp.task('images', function() {
+  return gulp.src([
+    paths.images
+  ])
+  .pipe(livereload());
+});
+
+gulp.task('server', function() {
+  return gulp.src([
+    paths.server
+  ])
+  .pipe(jshint())
+  .pipe(jshint.reporter(stylish));
+});
+
+
 gulp.task('react', bundle);
+gulp.task('watch', function () {
+  livereload.listen();
+
+  gulp.watch(paths.html,['html']);
+  gulp.watch(paths.images,['images']);
+  gulp.watch(paths.styles,['styles']);
+  gulp.watch(paths.reactScripts,['react']);
+  gulp.watch(paths.server,['server']);
+});
+
 
 // Runs watchify again on any changes
 b.on('update', bundle);
 b.on('log', gutil.log);
+gulp.task('default', ['watch', 'react']);
