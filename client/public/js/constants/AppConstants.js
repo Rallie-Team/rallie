a2a3d4ea17a4da71b1b696dc4ecac65436eb852d
Steven Shyun <stevenshuhyow@yahoo.com>
2015-05-27 12:02:17 -0700

    Added documentation for App Constants.js

--- client/public/js/constants/AppConstants.js
+++ client/public/js/constants/AppConstants.js
@@ -3,6 +3,9 @@
 // keymirror creates an object with values equal to its key names
 var keyMirror = require('keymirror');
 
+//AppConstants is referenced from the files in the actions folder
+//This creates modularity for how events are referenced and
+//creates a list of all events in the app.
 module.exports = keyMirror({
   EVENT_CREATE: null,
   EVENT_DELETE: null,
