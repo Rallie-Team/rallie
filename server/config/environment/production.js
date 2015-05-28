0b9df25d9a3c5e479a9c3cf3694f54d9ae5232f3
Kevin Huang <huang.kev@gmail.com>
2015-05-27 19:17:24 -0700

    Connected database with server

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,1 +1,7 @@
 // TODO: Create production environment database connection
+module.exports = {
+  postgres: {
+    // The DATABASE_URL variable should already include the username, password, host, port, and database name
+    uri: process.env.DATABASE_URL
+  }
+};
--- server/db/index.js
+++ server/db/index.js
@@ -1,91 +1,93 @@
 var Sequelize = require('sequelize');
+var config = require('../config/environment');
 
-var orm;
-if (process.env.DATABASE_URL) {
-  // The DATABASE_URL variable should already include the username, password, host, port, and database name
-  orm = new Sequelize(process.env.DATABASE_URL);
-} else {
-  orm = new Sequelize('joseki', {
-    host: 'localhost',
-    dialect: 'postgres'
+var User, Event, Observation;
+
+var init = function() {
+  var orm;
+  if (config.postgres.uri) {
+    orm = new Sequelize(config.postgres.uri, config.postgres.options);
+  } else {
+    orm = new Sequelize(config.postgres.database, config.postgres.user, config.postgres.password, config.postgres.options);
+  }
+
+  User = orm.define('User', {
+    username: Sequelize.STRING,
+    facebook_id: Sequelize.STRING
   });
-}
 
-var User = orm.define('User', {
-  username: Sequelize.STRING,
-  facebook_id: Sequelize.STRING
-});
+  Event = orm.define('Event', {
+    eventName: Sequelize.STRING,
+    start: Sequelize.DATE,
+    end: Sequelize.DATE,
+    location: Sequelize.STRING,
+    minParticipants: Sequelize.INTEGER,
+    maxParticipants: Sequelize.INTEGER,
+    action: Sequelize.STRING
+  });
 
-var Event = orm.define('Event', {
-  eventName: Sequelize.STRING,
-  start: Sequelize.DATE,
-  end: Sequelize.DATE,
-  location: Sequelize.STRING,
-  minParticipants: Sequelize.INTEGER,
-  maxParticipants: Sequelize.INTEGER,
-  action: Sequelize.STRING
-});
+  Observation = orm.define('Observation', {
+    content: Sequelize.STRING,
+    completed: Sequelize.BOOLEAN
+  });
 
-var Observation = orm.define('Observation', {
-  content: Sequelize.STRING,
-  completed: Sequelize.BOOLEAN
-});
+  /**
+   * MODEL RELATIONS
+   * belongsToMany connects a source with multiple targets, and the targets can connect to multiple sources
+   * Using a `through` option will create a new model with foreign keys for the source and target
+   *
+   * Example:
+   * User.belongsToMany(Event, {as: 'ShepherdEvents', through: 'ShepherdEvent'});
+   * Event.belongsToMany(User, {as: 'Shepherds', through: 'ShepherdEvent'});
+   *
+   * We create a many-to-many relationship between users and events, and the join table is called ShepherdEvent.
+   * The ShepherdEvent table will have a foreign key for EventId and UserId.
+   * The User and Event models will have get, set, and add methods. For example, user.getShepherdEvents() 
+   * will return all instances of a ShepherdEvent belonging to that user, and each instance would reference the 
+   * Event the user is a shepherd for.
+   * 
+   * See http://sequelize.readthedocs.org/en/latest/docs/associations/#belongs-to-many-associations
+   *
+   * hasMany connects a source with multiple targets, but the targets can only connect to one source
+   *
+   * See http://sequelize.readthedocs.org/en/latest/docs/associations/#one-to-many-associations
+   */
 
-/**
- * MODEL RELATIONS
- * belongsToMany connects a source with multiple targets, and the targets can connect to multiple sources
- * Using a `through` option will create a new model with foreign keys for the source and target
- *
- * Example:
- * User.belongsToMany(Event, {as: 'ShepherdEvents', through: 'ShepherdEvent'});
- * Event.belongsToMany(User, {as: 'Shepherds', through: 'ShepherdEvent'});
- *
- * We create a many-to-many relationship between users and events, and the join table is called ShepherdEvent.
- * The ShepherdEvent table will have a foreign key for EventId and UserId.
- * The User and Event models will have get, set, and add methods. For example, user.getShepherdEvents() 
- * will return all instances of a ShepherdEvent belonging to that user, and each instance would reference the 
- * Event the user is a shepherd for.
- * 
- * See http://sequelize.readthedocs.org/en/latest/docs/associations/#belongs-to-many-associations
- *
- * hasMany connects a source with multiple targets, but the targets can only connect to one source
- *
- * See http://sequelize.readthedocs.org/en/latest/docs/associations/#one-to-many-associations
- */
+  User.belongsToMany(Event, {as: 'ShepherdEvents', through: 'ShepherdEvent'});
+  Event.belongsToMany(User, {as: 'Shepherds', through: 'ShepherdEvent'});
+  User.belongsToMany(Event, {as: 'SheepEvents', through: 'SheepEvent'});
+  Event.belongsToMany(User, {as: 'Sheep', through: 'SheepEvent'});
+  User.hasMany(Observation); // UserId on Observation
+  Event.hasMany(Observation); // EventId on Observation
 
-User.belongsToMany(Event, {as: 'ShepherdEvents', through: 'ShepherdEvent'});
-Event.belongsToMany(User, {as: 'Shepherds', through: 'ShepherdEvent'});
-User.belongsToMany(Event, {as: 'SheepEvents', through: 'SheepEvent'});
-Event.belongsToMany(User, {as: 'Sheep', through: 'SheepEvent'});
-User.hasMany(Observation); // UserId on Observation
-Event.hasMany(Observation); // EventId on Observation
-
-/* EXAMPLE OF HOW TO CREATE A USER, THEN AN EVENT, AND FINALLY AN OBSERVATION TIED TO THE USER AND EVENT:
-User.create({
-  username: 'Kev'
-}).then(function(user) {
-  Event.create({
-    eventName: 'Dance party',
-    location: 'San Francisco'
-  }).then(function(event) {
-    Observation.create({
-      content: 'It is poppin in herrr',
-      completed: false
-    }).then(function(observation) {
-      // Associate the observation to the event
-      return event.addObservation(observation);
-    }).then(function(observation) {
-      // Associate the observation to the user
-      user.addObservation(observation);
+  /* EXAMPLE OF HOW TO CREATE A USER, THEN AN EVENT, AND FINALLY AN OBSERVATION TIED TO THE USER AND EVENT:
+  User.create({
+    username: 'Kev'
+  }).then(function(user) {
+    Event.create({
+      eventName: 'Dance party',
+      location: 'San Francisco'
+    }).then(function(event) {
+      Observation.create({
+        content: 'It is poppin in herrr',
+        completed: false
+      }).then(function(observation) {
+        // Associate the observation to the event
+        return event.addObservation(observation);
+      }).then(function(observation) {
+        // Associate the observation to the user
+        user.addObservation(observation);
+      });
     });
   });
-});
-*/
+  */
 
-// sync all models
-orm.sync();
+  // sync all models
+  orm.sync();
+};
 
 // export all models
 exports.User = User;
 exports.Event = Event;
 exports.Observation = Observation;
+exports.init = init;
