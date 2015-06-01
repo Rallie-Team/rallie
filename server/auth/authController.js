eaa6699463c09cbf62667a94662fe00184dac82a
Steven Shyun <stevenshuhyow@yahoo.com>
2015-06-01 12:29:12 -0700

    Restored comments

--- server/auth/authController.js
+++ server/auth/authController.js
@@ -5,12 +5,10 @@ var config = require('../config/environment');
 
 module.exports = function(passport){
 
-  //The Facebook strategy allows users to log in to a web
-  //application using their Facebook account.
+  // Facebook strategy is the primary way for a user to log in the service
   passport.use(new FacebookStrategy({
 
-  //clientID and clientSecret need to remain private
-  //we need to add this information to heroku
+    // Uses configuration file to set Facebook auth parameters
   clientID: config.facebook.id,
   clientSecret: config.facebook.secret,
   callbackURL: config.facebook.callback
@@ -21,20 +19,28 @@ module.exports = function(passport){
     db.User.find({where: {'facebook_id' : profile.id}})
       .then(function(user){
         if(!user){
+          // Creates a new user which extracts the Facbook ID and username
+          // Facebook ID is referenced so that users are not able to sign up for more than one account
           // console.log('creating new user');
+
           db.User.create({
             username: profile.name.givenName,
             facebook_id: profile.id
           })
           .then(function(newUser){
             // console.log(newUser, '----------------------');
+            // null value represents that there were no errors
             return done(null, newUser);
           });
         } else {
+
+          // Current user exists and create currentUser object
+
           var currentUser = {
             username: user.username,
             table_id: user.id
           };
+           // null value represents that there were no errors
           return done(null, currentUser);
         }
       });
