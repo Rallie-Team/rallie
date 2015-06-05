022143ade05c12c9e0dc4803edaec1b45558a32a
Kevin Huang <huang.kev@gmail.com>
2015-06-04 21:43:40 -0700

    Updated tests for /api/event endpoints based on recent changes to the endpoints

--- server/test/events.spec.js
+++ server/test/events.spec.js
@@ -5,91 +5,58 @@ var app = require('../server.js');
 var db = require('../db');
 var expect = require('chai').expect;
 
-var eventInstance, eventId, userInstance, userId, sheepId;
+var eventInstance, eventId, shepherdInstance, shepherdId, sheepInstance, sheepId;
 
-// Create test user and event in db before tests
+// Create test shepherd, sheep, and event in db before tests
 before(function(done) {
-  db.init().then(function() {
+  db.init().then(function() {  
+    // Create shepherd  
     db.User.create({
       username: 'Sheepish Shepherd'
-    }).then(function (user) {
-      userInstance = user;
-      userId = user.id;
+    }).then(function (shepherd) {
+      // Create event
+      shepherdInstance = shepherd;
+      shepherdId = shepherd.id;
       var currentDate = new Date();
-      db.Event.create({
+      return db.Event.create({
         name: 'Herding cats',
         start: currentDate,
         end: new Date(currentDate.getTime() + 60 * 60 * 24 * 1000),
         location: 'San Francisco',
         minParticipants: 1,
         maxParticipants: 10
-      }).then(function (event) {
-        eventInstance = event;
-        eventId = event.id;
-        user.addShepherdEvent(event).then(function(shepherdEvent) {
-          event.addShepherd(user).then(function(){
-            done();
-          });
-        });
       });
+    }).then(function (event) {
+      // Add shepherd to event
+      eventInstance = event;
+      eventId = event.id;
+      return shepherdInstance.addShepherdEvent(event);
+    }).then(function() {
+      // Create sheep
+      return db.User.create({
+        username: 'Lonely Sheep'
+      });
+    }).then(function(sheep) {
+      sheepInstance = sheep;
+      sheepId = sheep.id;
+      done();
     });
-    // db.User.create({
-    //   username: 'Shepherdish Sheep'
-    // }).then(function (user) {
-    //   userInstance = user;
-    //   sheepId = user.id;
-    //   db.Event.findOne({
-    //     where: {
-    //       id: eventId
-    //     }
-    //   }).then(function (event) {
-    //     if (event) {
-    //       event.addSheep(sheepId).then(function () {
-    //         done();
-    //       });
-    //       // event.addSheep(sheep).then(function (SheepEvent) {
-    //       //   user.addSheepEvent(event).then(function () {
-    //       // res.json(event);
-    //     });
-    //   });
-    // });
   });
 });
 
-// Delete test user and event from db after tests
+// Delete test shepherd, sheep, and event from db after tests
 after(function(done) {
   eventInstance.destroy().then(function() {
-    userInstance.destroy().then(function() {
-      done();
+    shepherdInstance.destroy().then(function() {
+      sheepInstance.destroy().then(function () {
+        done();
+      });
     });
   });
 });
 
 describe('Event API Endpoints', function() {
   var basePath = '/api/event';
-
-  describe(basePath, function() {
-    it('Should return 200 on GET ' + basePath, function(done) {
-      request(app)
-        .get(basePath)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    });
-
-    it('Should return an array on GET ' + basePath, function(done) {
-      request(app)
-        .get(basePath)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect(function(res) {
-          if (!Array.isArray(res.body))
-            throw new Error('Expected an array, received ' + JSON.stringify(res.body));
-        })
-        .end(done);
-    });
-  });
 
   describe(basePath + '/:eventId', function() {
     it('Should return the correct event on GET for a valid eventId', function(done) {
@@ -119,6 +86,12 @@ describe('Event API Endpoints', function() {
         .end(done);
     });
 
+    it('Should return 400 on GET for a non-numeric eventId', function(done) {
+      request(app)
+        .get(basePath + '/event1')
+        .expect(400, done);
+    });
+
     it('Should update event details on PUT for an valid eventId', function(done) {
       var eventDetails = {name: 'Herding dogs', location: 'San Francisco'};
       request(app)
@@ -142,12 +115,33 @@ describe('Event API Endpoints', function() {
         .send(eventDetails)
         .expect(400, done);
     });
+
+    it('Should return 400 on PUT for a non-numeric eventId', function(done) {
+      request(app)
+        .put(basePath + '/event1')
+        .expect(400, done);
+    });
   });
 
-  describe(basePath + '/user/:userId', function() {
+  describe(basePath + '/sheep', function() {
+    it('Should return an array on GET', function(done) {
+      request(app)
+        .get(basePath + '/sheep')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function(res) {
+          if (!Array.isArray(res.body))
+            throw new Error('Expected an array, received ' + JSON.stringify(res.body));
+        })
+        .end(done);
+    });
+  });
+
+  describe(basePath + '/shepherd/:userId', function() {
     it('Should return an array on GET for a valid userId', function(done) {
       request(app)
-        .get(basePath + '/user/' + userId)
+        .get(basePath + '/shepherd/' + shepherdId)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200)
@@ -158,30 +152,39 @@ describe('Event API Endpoints', function() {
         .end(done);
     });
 
-    it('Should return a user\'s event on GET for a valid userId', function(done) {
+    it('Should include a user\'s event on GET for a valid userId', function(done) {
       request(app)
-        .get(basePath + '/user/' + userId)
+        .get(basePath + '/shepherd/' + shepherdId)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200)
         .expect(function(res) {
-          if (res.body[0].id !== eventId) {
-            throw new Error('Expected eventId ' + eventId + ', received ' + res.body[0].id);
-          }
+          var events = res.body;
+          var usersEvents = events.filter(function(event) {
+            return event.id === eventId;
+          });
+          if (!usersEvents.length)
+            throw new Error('Expected eventId ' + eventId + ' within array, received ' + JSON.stringify(events));
         })
         .end(done);
     });
 
     it('Should return a 400 on GET for an invalid userId', function(done) {
       request(app)
-        .get(basePath + '/user/0')
+        .get(basePath + '/shepherd/0')
+        .expect(400, done);
+    });
+
+    it('Should return a 400 on GET for a non-numeric userId', function(done) {
+      request(app)
+        .get(basePath + '/shepherd/shepherd1')
         .expect(400, done);
     });
   });
 
   describe(basePath + '/create', function() {
     it('Should create a new event on POST', function(done) {
-      var newEvent = {name: 'Cat dance party', location: 'The Cat House', start: '1970-01-01T00:00:00.000Z', userId: userId};
+      var newEvent = {name: 'Cat dance party', location: 'The Cat House', start: '1970-01-01T00:00:00.000Z', userId: shepherdId};
       request(app)
         .post(basePath + '/create')
         .set({'Content-Type': 'application/json', Accept: 'application/json'})
@@ -220,6 +223,15 @@ describe('Event API Endpoints', function() {
         .send(newEvent)
         .expect(400, done);
     });
+
+    it('Should not create a new event on POST if a non-numeric userId is supplied', function(done) {
+      var newEvent = {name: 'Cat dance party', location: 'The Cat House', start: '1970-01-01T00:00:00.000Z', userId: 'user1'};
+      request(app)
+        .post(basePath + '/create')
+        .set('Content-Type', 'application/json')
+        .send(newEvent)
+        .expect(400, done);
+      });
   });
 
   describe(basePath + '/add-participant/:eventId', function() {
@@ -227,12 +239,12 @@ describe('Event API Endpoints', function() {
       request(app)
         .post(basePath + '/add-participant/' + eventId)
         .set('Content-Type', 'application/json')
-        .send({id: userId})
+        .send({id: sheepId})
         .expect(200)
         .expect('Content-Type', /json/)
         .expect(function(res) {
-          if (res.body.id !== userId)
-            throw new Error('Expected sheep ID ' + userId + ', received ' + res.body.id);
+          if (res.body.id !== sheepId)
+            throw new Error('Expected sheep ID ' + sheepId + ', received ' + res.body.id);
         })
         .end(done);
     });
@@ -241,28 +253,44 @@ describe('Event API Endpoints', function() {
       request(app)
         .post(basePath + '/add-participant/0')
         .set('Content-Type', 'application/json')
-        .send({id: userId})
+        .send({id: sheepId})
         .expect(400, done);
     });
 
-    it('Should not add a participant to an event on POST for an invalid userId but valid eventId', function(done) {
+    it('Should not add a participant to an event on POST for an invalid sheepId but valid eventId', function(done) {
       request(app)
         .post(basePath + '/add-participant/' + eventId)
         .set('Content-Type', 'application/json')
         .send({id: 0})
         .expect(400, done);
     });
-  });
 
-  describe(basePath + '/remove-participant/:eventId', function() {
-    it('Should remove a participant from an event on DELETE for a valid userId and eventId', function(done) {
+    it('Should not add a participant to an event on POST for a non-numeric sheepId', function(done) {
       request(app)
         .post(basePath + '/add-participant/' + eventId)
         .set('Content-Type', 'application/json')
-        .send({id: userId})
+        .send({id: 'sheep1'})
+        .expect(400, done);
+    });
+
+    it('Should not add a participant to an event on POST for a non-numeric eventId', function(done) {
+      request(app)
+        .post(basePath + '/add-participant/event1')
+        .set('Content-Type', 'application/json')
+        .send({id: sheepId})
+        .expect(400, done);
+    });
+  });
+
+  describe(basePath + '/remove-participant/:eventId', function() {
+    it('Should remove a participant from an event on DELETE for a valid sheepId and eventId', function(done) {
+      request(app)
+        .post(basePath + '/add-participant/' + eventId)
+        .set('Content-Type', 'application/json')
+        .send({id: sheepId})
         .expect(200)
         .expect(function(res) {
-          if (res.body.id !== userId)
+          if (res.body.id !== sheepId)
             throw new Error('');
         })
         .end(function(err) {
@@ -272,11 +300,11 @@ describe('Event API Endpoints', function() {
             request(app)
               .del(basePath + '/remove-participant/' + eventId)
               .set('Content-Type', 'application/json')
-              .send({id: userId})
+              .send({id: sheepId})
               .expect(200)
               .expect(function(res) {
-                if (res.body.id !== userId)
-                  throw new Error('Expected userId ' + userId + ', received ' + res.body.id);
+                if (res.body.id !== sheepId)
+                  throw new Error('Expected userId ' + sheepId + ', received ' + res.body.id);
               })
               .end(done);
           }
@@ -295,7 +323,7 @@ describe('Event API Endpoints', function() {
       request(app)
         .del(basePath + '/remove-participant/0')
         .set('Content-Type', 'application/json')
-        .send({id: userId})
+        .send({id: sheepId})
         .expect(400, done);
     });
 
@@ -304,6 +332,22 @@ describe('Event API Endpoints', function() {
         .del(basePath + '/remove-participant/' + eventId)
         .set('Content-Type', 'application/json')
         .send({id: 0})
+        .expect(400, done);
+    });
+
+    it('Should not remove a participant from an event on DELETE for a non-numeric userId', function(done) {
+      request(app)
+        .del(basePath + '/remove-participant/' + eventId)
+        .set('Content-Type', 'application/json')
+        .send({id: 'sheep1'})
+        .expect(400, done);
+    });
+
+    it('Should not remove a participant from an event on DELETE for a non-numeric eventId', function(done) {
+      request(app)
+        .del(basePath + '/remove-participant/event1')
+        .set('Content-Type', 'application/json')
+        .send({id: sheepId})
         .expect(400, done);
     });
   });
