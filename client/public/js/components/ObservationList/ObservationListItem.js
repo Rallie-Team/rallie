81ae119435392c804c6dce5005e296ad504a16b2
Kevin Huang <huang.kev@gmail.com>
2015-06-05 18:18:24 -0700

    Formatted observation list items, use modal for observation images

--- client/public/js/components/ObservationList/ObservationListItem.js
+++ client/public/js/components/ObservationList/ObservationListItem.js
@@ -1,21 +1,38 @@
 var React = require('react');
 
-//EventList item is referenced in EventList.js
-//This will render all of the events in the event List
-//this.props references the event that is passed in
+// Render each observation from the observation collection
+// this.props.observation references the observation that is passed in from ObservationList
 var ObservationListItem = React.createClass({
-
   render: function() {
     return (
-      <div className="event-list-item">
-        <div className="event-list-item-name">{this.props.observation.User.username}</div>
-        <div className="list-item-datetime">{this.props.observation.createdAt}</div>
-        <div className="event-list-item-location">{this.props.observation.content}</div>
-        <img src={this.props.observation.rawImage}/>
+      <div className="observation-list-item">
+        <div className="observation-list-item-user">{this.props.observation.User.username}</div>
+        <div className="observation-list-item-datetime">{moment(this.props.observation.createdAt).format('llll')} - {moment(this.props.observation.createdAt).fromNow()}</div>
+        <div className="observation-list-item-text">{this.props.observation.content}</div>
+        { this.props.observation.rawImage ? 
+        <div className="observation-list-item-image center-block">
+          <img className="img-circle img-border center-block img-thumbnail" src={this.props.observation.rawImage} onClick={this.showModalImage.bind(this, this.props.observation.id)}/>
+          <div className="modal fade" id={'img' + this.props.observation.id} tabIndex="-1" role="dialog" aria-labelledby="observationImage" aria-hidden="true">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div className="modal-body">
+                  <img className="center-block" src={this.props.observation.rawImage}/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> : null
+        }
       </div>
     );
-  }
+  },
 
+  showModalImage: function(id) {
+    $('#img' + id).modal('show');
+  }
 });
 
 module.exports = ObservationListItem;
