7fff55b05941e637e77e7d660d4d807e13ed8f98
Steven Shyun <stevenshuhyow@yahoo.com>
2015-06-10 19:42:28 -0700

    Set up intial landing page.

--- Gulpfile.js
+++ Gulpfile.js
@@ -36,17 +36,17 @@ var bundle = function() {
 
 var paths = {
   // images: 'client/public/assets/img/*.*',
-  // styles: 'client/public/assets/css/*.css',
+  styles: 'client/public/assets/css/*.css',
   html: 'client/public/index.html',
   server: 'server/**/*.js'
 };
 
-// gulp.task('styles', function() {
-//   return gulp.src([
-//     paths.styles
-//   ])
-//   .pipe(livereload());
-// });
+gulp.task('styles', function() {
+  return gulp.src([
+    paths.styles
+  ])
+  .pipe(livereload());
+});
 
 gulp.task('html', function() {
   return gulp.src(
@@ -55,12 +55,12 @@ gulp.task('html', function() {
   .pipe(livereload());
 });
 
-// gulp.task('images', function() {
-//   return gulp.src([
-//     paths.images
-//   ])
-//   .pipe(livereload());
-// });
+gulp.task('images', function() {
+  return gulp.src([
+    paths.images
+  ])
+  .pipe(livereload());
+});
 
 gulp.task('server', function() {
   return gulp.src([
@@ -77,8 +77,8 @@ gulp.task('watch', function () {
   livereload.listen();
 
   gulp.watch(paths.html,['html']);
-  // gulp.watch(paths.images,['images']);
-  // gulp.watch(paths.styles,['styles']);
+  gulp.watch(paths.images,['images']);
+  gulp.watch(paths.styles,['styles']);
   gulp.watch(paths.server,['server']);
 });
 
